664e3f0d678b5d447d091d07dc3f77981c54d21d
rhsabbir1 <rhsabbir090@gmai.com>
2023-03-08 20:28:11 +0600

    deposite fixing done

--- js/deposit.js
+++ js/deposit.js
@@ -6,6 +6,7 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     3. convert string value to a number
     */
   const newDepositAmount = getInputFieldValueById("deposit-field");
+  
   /* 
     1. get previous deposit total by id
 
@@ -14,12 +15,17 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
 
   // calculate new deposit total
   const newDepositTotal = previousDepositTotal + newDepositAmount;
-  console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+console.log(newDepositTotal)
+  // console.log(newDepositTotal, previousDepositTotal, newDepositAmount);
+
   // set deposit total value
+
   setTextElementValueById("deposit-total", newDepositTotal);
 
   // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
+  const previousBalanceTotal = getTextElementValueById("balance-total");
   const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+ 
   setTextElementValueById("balance-total", newBalanceTotal);
-);
+  
+});
